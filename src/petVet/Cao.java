b0fcc8cf5510fb66511633ebc3d28d8027a22219
GiovaneVerbinnen <gioverbinnen@gmail.com>
2021-06-28 22:49:57 -0300

    initial commit

---
+++ src/petVet/Cao.java
@@ -0,0 +1,15 @@
+package petVet;
+
+public class Cao extends Mamifero {
+	
+	public Cao(String nome, int idade, String dono, String cuidadosProprios, 
+			String alimentacao) {
+		super(nome, idade, dono, cuidadosProprios, alimentacao);
+		this.especie = "Cachorro";
+	}
+	private static final long serialVersionUID = 1L;
+
+	public String soar() {
+		return "Faz latidos";
+	}
+}
---
+++ src/petVet/Cavalo.java
@@ -0,0 +1,17 @@
+package petVet;
+
+public class Cavalo extends Mamifero{
+
+	public Cavalo(String nome, int idade, String dono, String cuidadosProprios, String alimentacao) {
+		super(nome, idade, dono, cuidadosProprios, alimentacao);
+		this.especie = "Cavalo";
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String soar() {
+		// TODO Auto-generated method stub
+		return "Relincha";
+	}
+}
